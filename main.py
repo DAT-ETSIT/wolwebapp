c899db9dd8259348ba5caa1f03834fe083501fae
Pablofl01 <pablofl01@gmail.com>
2022-11-16 01:26:03 +0100

    Implemented machine addition button

--- main.py
+++ main.py
@@ -18,17 +18,16 @@ def index():
     elif request.method == 'POST':
         new_machines = []
         for i in range(0,int((len(request.form)-1)/3)):
-            new_machines.append({"name": request.form[f"machine[{i}][name]"], "mac": request.form[f"machine[{i}][mac]"], "ip": request.form[f"machine[{i}][ip]"], "port": "7"})
+            if request.form[f"machine[{i}][name]"] != "" and request.form[f"machine[{i}][mac]"] != "" and request.form[f"machine[{i}][ip]"] != "":
+                new_machines.append({"name": request.form[f"machine[{i}][name]"], "mac": request.form[f"machine[{i}][mac]"], "ip": request.form[f"machine[{i}][ip]"], "port": "7"})
         with open('./machines.json', 'w') as file:
             file.write(json.dumps(new_machines, ensure_ascii=False))
-            
         return redirect(url_for('index'))
 
 @app.route('/edit')
 def machineEdit():
     with open('./machines.json', 'r') as file:
         machines = json.load(file)
-    
     return render_template('editMachines.html', machines=machines)
 
 @app.route('/wol/<int:machineId>')
