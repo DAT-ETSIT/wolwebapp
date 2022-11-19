7ad7ef6df5009fd23c8feeb85f4eb76fc4c4cbf9
pablofl01 <pablofl01@gmail.com>
2022-11-19 21:33:03 +0100

    Updated installation script

--- install.sh
+++ install.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
-	then echo "No se ha indicado nombre de host. Ejemplo: 'sudo ./install.sh hostname.example.com'"
-	exit
-fi
-
 if [ "$EUID" -ne 0 ] 
 	then echo "Para poder realizar la instalación, debes ejecutar este script como root."
 	exit
 fi
 
-echo "Instalando dependencias necesarias"
+if [ $# -ne 2 ]
+	then echo "El número de parámetros especificados no es correcto, deben incluirse la dirección y el puerto de escucha. Ejemplo: 'sudo ./install.sh hostname.example.com 8080'"
+	exit
+fi
+
+path="$(pwd -P | sed -r 's/\//\\\//g')"
+
+echo "Comprobando las dependencias necesarias."
+apt update
+
+for check in ethtool python3 python3-pip
+    do
+        result=`dpkg-query -s $check 2>&1`
+        if [[ "$result" == *"is not installed"* ]]; then
+            apt-get -y install $check
+        fi
+    done
+
 pip3 install -r ./requirements.txt
 
 echo "Configurando servicio"
-sed "s/hostname/$1/g" ./wolsimpleserverExample.service > /etc/systemd/system/wolsimpleserver.service
+
+sed -e "s/path/$path/g; s/hostname/$1/g; s/portNumber/$2/g" ./wolsimpleserverExample.service > /etc/systemd/system/wolsimpleserver.service
 
 echo "Iniciando servicio"
+sudo systemctl enable wolsimpleserver.service
 sudo systemctl start wolsimpleserver.service
 
 echo "Instalación completada"
