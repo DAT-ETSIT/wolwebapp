27f37c4c98be410ca4d65ec86403efd2362276bd
pablofl01 <pablofl01@gmail.com>
2023-01-15 00:49:26 +0100

    Changed WSGI to Gunicorn

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import os
+
+from main import app
+
+if __name__ == '__main__':
+    PORT = os.environ.get('PORT') if os.environ.get('PORT') else 3000
+
+    if os.environ.get('ENV') == "development":
+        app.run(host='0.0.0.0', port=PORT, debug=True)
+    else:
+        app.run()
