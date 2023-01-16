244c3775cbe525dd2b21017b2c39d86a37dd0323
pablofl01 <pablofl01@gmail.com>
2023-01-16 23:58:52 +0100

    Updated update.sh

--- update.sh
+++ update.sh
@@ -5,8 +5,9 @@ cd $SCRIPT_DIR
 
 update() {
     read -r -p "Hay una nueva versión disponible, ¿actualizar? (S/N) " response
-    if [[ "$response" == "S" || "$response" == "s" ]]; then        
+    if [[ "$response" == "S" || "$response" == "s" ]]; then   
         git pull
+        systemctl restart wolsimpleserver.service
     else
         echo "Se ha cancelado la operación"
         exit()
